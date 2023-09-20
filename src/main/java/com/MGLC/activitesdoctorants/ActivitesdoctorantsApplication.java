69d5ec012b84229c65550dede620fc4dd2f788ee
madouchhicham <madouchhicham@gmail.com>
2023-09-20 21:05:51 +0100

    GLCC Project

---
+++ src/main/java/com/MGLC/activitesdoctorants/ActivitesdoctorantsApplication.java
@@ -0,0 +1,13 @@
+package com.MGLC.activitesdoctorants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitesdoctorantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitesdoctorantsApplication.class, args);
+	}
+
+}
