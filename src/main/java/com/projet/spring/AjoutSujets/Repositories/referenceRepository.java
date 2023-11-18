7193f78fb99105a1a249d5e2b1a6a1527e86a330
hassna.rahmouni <hassna.rahmouni@uit.ac.ma>
2023-11-18 23:01:11 +0000

    Modifications

--- src/main/java/com/projet/spring/AjoutSujets/Repositories/referenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projet.spring.AjoutSujets.Repositories;
-
-import com.projet.spring.AjoutSujets.Entities.reference;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface referenceRepository extends JpaRepository<reference,Long> {
-}
